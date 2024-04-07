8ec4598233a1c360e3d28f25f32e5c2ccccb9085
Craig Murray <cmurray4492@gmail.com>
2024-04-06 19:27:25 -0500

    first model commit

--- center/models.py
+++ center/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Center(models.Model):
+    name = models.CharField(max_length=124)
+    address = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
--- mysite/settings.py
+++ mysite/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "vaccine.apps.VaccineConfig",
     "user.apps.UserConfig",
-    "centger.apps.CenterConfig",
+    "center.apps.CenterConfig",
     "campaign.apps.CampaignConfig",
-    "vaccination.apps.Vaccation.apps.VaccinationConfig",
+    "vaccination.apps.VaccinationConfig",
 ]
 
 MIDDLEWARE = [
